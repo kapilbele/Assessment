c2cdaedb4fbb1fb520532500d56cacfbb5997334
kapil <motirambele06@gmail.com>
2021-01-11 19:18:38 +0530

    Done with Inventory suite and external wait

--- assessment2/src/main/java/testscripts/TestDeleteProduct.java
+++ assessment2/src/main/java/testscripts/TestDeleteProduct.java
@@ -11,30 +11,45 @@ import org.testng.annotations.Test;
 import library.BaseTest;
 import pageobject.AddToCartPage;
 
-public class TestAddToCart extends BaseTest
+public class TestDeleteProduct extends BaseTest
 {
 	@Test()
-	public void addtocartmethod()
+	public void delete()
 	{
 		try
 		{
-			
 			AddToCartPage acp = new AddToCartPage(driver);
 			acp.searchbox().clear();
 			acp.searchbox().sendKeys(searchinput);
 			acp.searchbox().sendKeys(Keys.ENTER);
 			acp.mobile().click();
+			Thread.sleep(5000);
+			
 			ArrayList<String> tab2 = new ArrayList<String>(driver.getWindowHandles());
 		    driver.switchTo().window(tab2.get(1));
 		    driver.navigate();
 			
 		    WebElement addtocart = driver.findElement(By.id("add-to-cart-button"));
 		    addtocart.click();
-			Reporter.log("product added into cart successfully....",true);
-		}
+		    Thread.sleep(5000);
+		    
+		    WebElement cartclose = driver.findElement(By.id("attach-close_sideSheet-link"));
+		    cartclose.click();
+		    Thread.sleep(5000);
+		    
+		   WebElement cartbox = driver.findElement(By.id("nav-cart-count"));
+		   cartbox.click();
+		   Thread.sleep(5000);
+		   
+		   WebElement deletebutton = driver.findElement(By.className("a-color-link"));
+		   deletebutton.click();
+		   Thread.sleep(5000);
+		   Reporter.log("product deleted successfully....",true);
+		   
+		} 
 		catch (Exception e) 
 		{
-			
+			System.out.println(e.getStackTrace());
 		}
 	}
 }
--- assessment2/src/main/java/testscripts/TestLogin.java
+++ assessment2/src/main/java/testscripts/TestLogin.java
@@ -12,31 +12,26 @@ import pageobject.AmazonSearchPage;
 
 public class TestLogin extends BaseTest
 {
-	@Test
+	@Test() 
 	public void amazonlogin()
 	{
 		try
 		{
 			AmazonSearchPage asp = new AmazonSearchPage(driver);
 			//asp.searchlink().click();
-			Thread.sleep(10000);
 			Actions action = new Actions(driver);
 			WebElement mousemoveonsignin = driver.findElement(By.id("nav-link-accountList-nav-line-1"));
 			action.moveToElement(mousemoveonsignin).build().perform();
 			mousemoveonsignin.click();
-			Thread.sleep(5000);
 			AmazonResultPage arp = new AmazonResultPage(driver);
 			//arp.login().click();
 			arp.username().clear();
 		
 			arp.username().sendKeys(username);
-			Thread.sleep(5000);
 			arp.continuebotton().click();
 			arp.password().clear();
 			arp.password().sendKeys(password);
-			Thread.sleep(5000);
 			arp.loginbutton().click();
-			Thread.sleep(5000);
 			Reporter.log("logged in successfully.....",true);
 		}
 		catch (Exception e) 
